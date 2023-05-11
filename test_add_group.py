bfd9b11cb593f2193aa9dc905b82d92382bc190a
ZamSerega <japona@mail.ru>
2023-05-11 22:48:56 +0300

    HOLY FUCKIN WORK

--- test_add_group.py
+++ test_add_group.py
@@ -6,43 +6,72 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from group import Group
 
-class UntitledTestCase(unittest.TestCase):
+class AddNewGroup(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
 
-    def test_untitled_test_case(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/group.php")
-        driver.find_element(By.NAME, "user").clear()
-        driver.find_element(By.NAME, "user").send_keys("admin")
-        driver.find_element(By.NAME, "pass").click()
-        driver.find_element(By.NAME, "pass").clear()
-        driver.find_element(By.NAME, "pass").send_keys("secret")
-        driver.find_element(By.XPATH, "//input[@value='Login']").click()
-        driver.find_element(By.LINK_TEXT, "groups").click()
-        driver.find_element(By.NAME, "new").click()
-        driver.find_element(By.NAME, "group_name").click()
-        driver.find_element(By.NAME, "group_name").clear()
-        driver.find_element(By.NAME, "group_name").send_keys("Faggot")
-        driver.find_element(By.NAME, "group_header").click()
-        driver.find_element(By.NAME, "group_header").clear()
-        driver.find_element(By.NAME, "group_header").send_keys("YOU")
-        driver.find_element(By.NAME, "group_footer").click()
-        driver.find_element(By.NAME, "group_footer").clear()
-        driver.find_element(By.NAME, "group_footer").send_keys("YOOOOUUUU")
-        driver.find_element(By.NAME, "submit").click()
-        driver.find_element(By.LINK_TEXT, "groups").click()
-        driver.find_element(By.LINK_TEXT, "Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
+    def test_add_new_group(self):
+        wd = self.wd
+        self.open_website(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups(wd)
+        self.create_new_group(wd, Group(name="Faggot", header="YOU", footer="YOOOOUUUU"))
+        self.return_to_groups(wd)
+        self.logout(wd)
+
+    def test_empty_group(self):
+        wd = self.wd
+        self.open_website(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups(wd)
+        self.create_new_group(wd, Group(name="", header="", footer=""))
+        self.return_to_groups(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        # Выход из браузера
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def return_to_groups(self, wd):
+        # Возвращение на страницу groups
+        wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def create_new_group(self, wd, group):
+        # Создание новой группы
+        wd.find_element(By.NAME, "new").click()
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        wd.find_element(By.NAME, "submit").click()
+
+    def open_groups(self, wd):
+        # Открытие страницы groups
+        wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def login(self, wd, username, password):
+        # Login
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys(username)
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys(password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_website(self, wd):
+        # Открываем сайт локал\адресбук
+        wd.get("http://localhost/addressbook/group.php")
 
     def tearDown(self):
-        self.driver.quit()
+        self.wd.quit()
 
 if __name__ == "__main__":
     unittest.main()
